b58d43402bca8e4f13acfbb615b7164334a40651
karpada2 <gongrossman@gmail.com>
2025-08-04 12:05:17 +0300

    made it use a normal time stamp

--- app/src/main/java/com/example/androidapplearning/MainActivity.kt
+++ app/src/main/java/com/example/androidapplearning/MainActivity.kt
@@ -22,6 +22,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.androidapplearning.ui.theme.AndroidAppLearningTheme
 import com.google.firebase.Firebase
 import com.google.firebase.database.database
+import java.util.Calendar
 
 private const val TAG = "MainActivity"
 private var numberDisplayValue: Int = 0
@@ -71,7 +72,7 @@ class MainActivity : ComponentActivity() {
                 override fun onClick(v: View?) {
                     val teamName: String = editTextTeamName.text.toString()
                     val matchInfo: TeamMatch = TeamMatch(teamName, numberDisplayValue)
-                    var databaseNewEntryRef = database.getReference(teamName).push()
+                    var databaseNewEntryRef = database.getReference(teamName).child(Calendar.getInstance().time.toString())
                     databaseNewEntryRef.setValue(matchInfo)
                 }
             }
